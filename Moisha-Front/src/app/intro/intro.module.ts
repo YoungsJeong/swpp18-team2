cf8ad86b7552fbe9930b9fb2ba657364019fbfa9
Jay Lee <onteeae@gmail.com>
2018-11-03 21:52:54 +0900

    implementing intro component

--- Moisha-Front/src/app/intro/intro.module.ts
+++ Moisha-Front/src/app/intro/intro.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntroComponent } from './intro/intro.component';
 import { SigninComponent } from './signin/signin.component';
+import {IntroRoutingModule} from './intro-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [IntroRoutingModule],
   declarations: [IntroComponent, SigninComponent]
 })
 export class IntroModule { }
--- Moisha-Front/src/app/intro/signin/signin.component.ts
+++ Moisha-Front/src/app/intro/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {AuthService} from '../../core/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  pending: boolean;
+  error: any;
+  submitAttempt: boolean;
+  signInForm: FormGroup;
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
+    this.createForm()
+  }
+  createForm() {
+    this.signInForm = new FormGroup(
+      {
+        email: new FormControl(
+          '',
+          Validators.compose([Validators.email, Validators.required])
+        ),
+        password: new FormControl('', Validators.required)
+      },
+      { updateOn: 'submit' }
+    );
   }
 
+  goToSignUp() {
+    console.log();
+    this.router.navigate(['/signup']);
+  }
+
+  login() {
+    this.submitAttempt = true;
+    if (this.signInForm.valid) {
+      this.pending = true;
+      this.error = false;
+      this.auth.login(this.formEmail.value, this.formPassword.value).subscribe({
+        error: err => {
+          console.error('signin error: ', err);
+          this.pending = false;
+          this.error = true;
+        },
+        complete: () => {
+          this.pending = false;
+        }
+      });
+    }
+  }
+
+  get formEmail() {
+    return this.signInForm.get('email');
+  }
+
+  get formPassword() {
+    return this.signInForm.get('password');
+  }
 }
